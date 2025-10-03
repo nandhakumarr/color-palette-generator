6e8dca8a0926f41f61b57c9438df125478f39dcf
Nandhakumar Ramanathan <nandhakumar.r@gmail.com>
2025-10-03 15:38:15 +0530

    feat: input ratio to generate colors

--- src/components/Palette.vue
+++ src/components/Palette.vue
@@ -15,12 +15,12 @@
 
   const variants = ref([
     { code: 'TINT', label: 'Tints' },
-    { code: 'TONE', label: 'Tones' },
-    { code: 'SHADE', label: 'Shades' },
-    { code: 'SATURATION', label: 'Saturations' },
-    { code: 'DESATURATION', label: 'Desaturations' },
     { code: 'LIGHTNESS', label: 'Lighter' },
+    { code: 'TONE', label: 'Tones' },
+    { code: 'DESATURATION', label: 'Desaturations' },
+    { code: 'SHADE', label: 'Shades' },
     { code: 'LIGHTNESS_NEGATIVE', label: 'Darker' },
+    { code: 'SATURATION', label: 'Saturations' },
   ])
 
   const selectedVariant = ref('TINT')
@@ -58,6 +58,12 @@
     if (!maxVariant[variant](colors, palette[variant][palette[variant].length - 1]))
       palette[variant].push(colors)
   }
+
+  watch(() => color.value, () => {
+    Object.keys(palette).forEach(key => {
+      palette[key] = []
+    })
+  })
 </script>
 
 <template lang="pug">
@@ -72,20 +78,38 @@
   .palette
     .variants
       .variant(v-for="variant in variants" :key="variant.code" @click="selectedVariant = variant.code" :class="{ active: selectedVariant === variant.code }")
-        h4 {{ variant.label }}
+        header
+          h4 {{ variant.label }}
+          input(type="range" min="0.025" max="1.0" v-model="ratios[variant.code]" step="0.025")
+          span {{ ratios[variant.code] * 100 }}%
         color-variant(:variants="palette[variant.code]" @add="addVariant(variant.code)")
 
 </template>
 
 <style lang="sass" scoped>
 .color-palette
-  display: block
+  height: 100%
+  overflow: hidden
+  display: flex
+  align-items: stretch
+  flex-direction: column
   padding: 0
 
-header
-  padding-bottom: 1rem
-  margin-bottom: 1rem
-  border-bottom: 1px solid #ccc
+  > header
+    padding-bottom: 1rem
+    margin-bottom: 1rem
+    border-bottom: 1px solid #ccc
+
+.palette
+  flex: 1
+  overflow: auto
+  header
+    display: flex
+    gap: 1rem
+    background: #f2f2f2
+    padding: 0.5rem
+    h4
+      margin: 0
 
 .inputs
   margin-top: 1rem
@@ -95,11 +119,6 @@ header
 .base-color
   display: flex
   gap: 2rem
-
-.variant h4
-  background: #f2f2f2
-  margin: 0
-  padding: 0.5rem
 
 .color
   text-align: center
--- src/utils/generator.js
+++ src/utils/generator.js
@@ -2,31 +2,31 @@ import color from 'color'
 
 export const generate = {
   TINT: (baseColor, ratio, step) => {
-    const _ratio = ratio * step
+    const _ratio = ratio * (step + 1)
     return color(baseColor).mix(color('white'), _ratio).hex()
   },
   TONE: (baseColor, ratio, step) => {
-    const _ratio = ratio * step
+    const _ratio = ratio * (step + 1)
     return color(baseColor).mix(color('gray'), _ratio).hex()
   },
   SHADE: (baseColor, ratio, step) => {
-    const _ratio = ratio * step
+    const _ratio = ratio * (step + 1)
     return color(baseColor).mix(color('black'), _ratio).hex()
   },
   SATURATION: (baseColor, ratio, step) => {
-    const _ratio = ratio * step
+    const _ratio = ratio * (step + 1)
     return color(baseColor).saturate(_ratio).hex()
   },
   DESATURATION: (baseColor, ratio, step) => {
-    const _ratio = ratio * step
+    const _ratio = ratio * (step + 1)
     return color(baseColor).desaturate(_ratio).hex()
   },
   LIGHTNESS: (baseColor, ratio, step) => {
-    const _ratio = ratio * step
+    const _ratio = ratio * (step + 1)
     return color(baseColor).lightness(color(baseColor).lightness() + (_ratio * 100)).hex()
   },
   LIGHTNESS_NEGATIVE: (baseColor, ratio, step) => {
-    const _ratio = ratio * step
+    const _ratio = ratio * (step + 1)
     return color(baseColor).lightness(color(baseColor).lightness() - (_ratio * 100)).hex()
   },
 }
